49d12b0a6969bd4fbe504cce6122408be1dcc18e
busrauludg <busrapc.226@gmail.com>
2024-12-25 21:26:45 +0300

    test2

--- ETicaret/ETicaret/obj/Debug/net8.0/ETicaret.AssemblyInfo.cs
+++ ETicaret/ETicaret/obj/Debug/net8.0/ETicaret.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETicaret")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adceb057227f381622670042353884b6286af7c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETicaret")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETicaret")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
